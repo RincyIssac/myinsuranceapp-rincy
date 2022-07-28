60103a1e687f671906ad1694e222ed0125cb7f00
Rincy Issac <rincy@allianz.com>
2022-07-28 13:31:18 +0200

    terraform config files added - infra as a code

--- terraform/aks-cluster.tf
+++ terraform/aks-cluster.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "default" {
-  name     = akshackathonRincy-rg
+  name     = "akshackathonRincy"
   location = "eastasia"
 
   tags = {
@@ -14,7 +14,7 @@ resource "azurerm_resource_group" "default" {
 }
 
 resource "azurerm_kubernetes_cluster" "default" {
-  name                = akshackathonRincy
+  name                = "akshackathonRincy"
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
   dns_prefix          = "${random_pet.prefix.id}-k8s"
