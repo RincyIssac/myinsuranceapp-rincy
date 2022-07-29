650d45f5ed1ad46ed40a68f989916dd606309b93
Rincy Issac <rincy@allianz.com>
2022-07-29 08:24:41 +0200

    update

--- tests/acceptance-request/test_app_request.py
+++ tests/acceptance-request/test_app_request.py
@@ -8,8 +8,8 @@ class TestApp(unittest.TestCase):
     base_url='http://localhost:5000/api/v1'
 
     def test_1_getToken(self):
-        url=f"{self.base_url}/token"
         test_data = {"email":"jd@myinsuranceapp.com","password":"passwordjd"}
+        url=f"{self.base_url}/token"
         response = requests.post(url, json=test_data)
         data=json.loads(response.text)
         print(f"post: {data}")
@@ -35,3 +35,33 @@ class TestApp(unittest.TestCase):
         data=json.loads(response.text)
         print(f"get_user_products: {data}")
         self.assertTrue(response.status_code > 400)
+
+    """def test_4_get_users(self):
+        url=f"{self.base_url}/users/1/products"
+        headers = {"Authorization": f"Bearer {TestApp.token}"}
+        response = requests.get('users/1/products', content_type='application/json', headers=headers)
+        data=json.loads(response.text)
+        print(f"get : {data}")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(len(data)>0)
+        self.assertIsNotNone(data[0] ["fullname"])"""
+
+    def test_4_post(self):
+        url=f"{self.base_url}/users/2/products"
+        headers = {"Authorization": f"Bearer {TestApp.token}"}
+        PRODUCT_ID = 3
+        test_data = {"id":PRODUCT_ID}
+        response = requests.post(url, json=test_data, headers=headers)
+        data=json.loads(response.text)
+        print(f"post: {data}")
+        self.assertEqual(response.status_code, 200)
+
+        '''def test_2_post(self):
+        url=f"{self.base_url}/customers/"
+        test_data = {"address": "5 Spenser Plaza",
+                     "email": "kbraksper0@mac.com",
+                     "name": "Kelbee"}
+        response = requests.post(url, json=test_data)
+        data=json.loads(response.text)
+        print(f"post: {data}")
+        self.assertEqual(response.status_code, 200)'''
